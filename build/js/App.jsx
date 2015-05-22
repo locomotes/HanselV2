e23a0ad65d69686e7171708689d2ea90908ff00e
Jon Dickerson <jongdickerson@gmail.com>
2015-05-22 16:16:09 -0400

    set up react project with gulp

---
+++ build/js/App.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+
+var Test = require('./Test.jsx');
+
+React.render(
+  <Test />, document.body
+);
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
-var gulp      = require('gulp');
-var plumber   = require('gulp-plumber');
-var sass      = require('gulp-sass');
-var webserver = require('gulp-webserver');
-var opn       = require('opn');
+var gulp       = require('gulp');
+var plumber    = require('gulp-plumber');
+var sass       = require('gulp-sass');
+var webserver  = require('gulp-webserver');
+var opn        = require('opn');
+var browserify = require('browserify');
+var source     = require('vinyl-source-stream');
+var reactify   = require('reactify');
 
 var sourcePaths = {
-  styles: ['scss/**/*.scss']
+  styles: ['./build/scss/**/*.scss']
 };
 
 var distPaths = {
-  styles: 'css'
+  styles: './public/css',
+  html: './public',
+  js: './public/js'
 };
 
 var server = {
@@ -21,17 +26,30 @@ gulp.task('sass', function () {
   gulp.src( sourcePaths.styles )
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest( distPaths.styles ));
+    .pipe(gulp.dest(distPaths.styles));
+});
+
+gulp.task('html', function() {
+  gulp.src('./build/index.html')
+    .pipe(gulp.dest(distPaths.html));
 });
 
 gulp.task('webserver', function() {
-  gulp.src( '.' )
+  gulp.src( './public' )
     .pipe(webserver({
       host:             server.host,
       port:             server.port,
       livereload:       true,
       directoryListing: false
     }));
+});
+
+gulp.task('js', function () {
+  browserify('./build/js/App.jsx')
+    .transform('reactify')
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest(distPaths.js))
 });
 
 gulp.task('openbrowser', function() {
@@ -42,6 +60,6 @@ gulp.task('watch', function(){
   gulp.watch(sourcePaths.styles, ['sass']);
 });
 
-gulp.task('build', ['sass']);
+gulp.task('build', ['html', 'js', 'sass']);
 
 gulp.task('default', ['build', 'webserver', 'watch', 'openbrowser']);
